00549a3278c3dab502fa949895bf5ae5fb2f0453
Joao Victor <joaovictormr91@gmail.com>
2021-08-26 20:53:13 -0300

    created products controller

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const path        = require('path');
-const adminData   = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes  = require('./routes/shop');
 const app         = express();
 
@@ -9,11 +9,11 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page not Found' });
+  res.status(404).render('404', { pageTitle: 'Page not Found', path: '' });
 });
 
 app.listen(3000);
