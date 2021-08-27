5c29dac26b200153b74b9f066754b4d289e2469b
Joao Victor <joaovictormr91@gmail.com>
2021-08-27 02:29:56 -0300

    controllers refactoring

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path        = require('path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes  = require('./routes/shop');
 const app         = express();
+const errorController = require('./controllers/error');
 
 app.set('view engine', 'ejs');
 
@@ -12,8 +13,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page not Found', path: '' });
-});
+app.use(errorController.get404Page);
 
 app.listen(3000);
