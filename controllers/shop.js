233b1d7bf79b053f408d71d9b07cf40cdab7a606
Joao Victor Martins <joaovictormr91@gmail.com>
2021-09-29 23:46:37 -0300

    orders page creation

--- controllers/shop.js
+++ controllers/shop.js
@@ -119,7 +119,9 @@ exports.getCheckout = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+    let fetchedCart;
     req.user.getCart().then(cart => {
+        fetchedCart = cart;
         return cart.getProducts();
     })
     .then(products => {
@@ -135,13 +137,20 @@ exports.postOrder = (req, res, next) => {
             }).catch(err => console.log(err));
     })
     .then(result => {
+        return fetchedCart.setProducts(null);
+    })
+    .then(result => {
         res.redirect('/orders');
     }).catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Orders'
-    })
+    req.user.getOrders()
+        .then(orders => {
+            res.render('shop/orders', {
+                path: '/orders',
+                pageTitle: 'Orders',
+                orders: orders
+            })
+        })  
 };
