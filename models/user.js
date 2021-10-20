8e220670d88012ad1dd4468689067cd40ebc3362
Joao Victor Martins <joaovictormr91@gmail.com>
2021-10-20 00:56:49 -0300

    Removed cart and cart-item models and inserted the cart update and creation in the user model

--- models/user.js
+++ models/user.js
@@ -3,14 +3,32 @@ const getDb     = require('../util/database').getDb;
 
 
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, id) {
         this.username = username;
         this.email = email;
+        this.cart = cart;
+        this._id = id;
     }
 
     save() {
         const db = getDb();
         return db.collection('users').insertOne(this);
+    }
+
+    addToCart(product) {
+        // const cartProducts = this.cart.items.findIndex(cp => {
+        //     return cp._id === product._id;
+        // });
+
+        const updatedCart = {items: [{...product, quantity: 1}]};
+        const db = getDb();
+        return db
+            .collection('users')
+            .updateOne(
+                { _id: mongodb.ObjectId(this._id) },
+                { $set: { cart: updatedCart } } 
+            );
+
     }
 
     static findById(userId) {
