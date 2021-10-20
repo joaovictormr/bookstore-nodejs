c7542ffa864030e606b7db6e786dc1403389ee92
Joao Victor Martins <joaovictormr91@gmail.com>
2021-10-19 23:50:34 -0300

    User model creation

--- app.js
+++ app.js
@@ -6,21 +6,24 @@ const shopRoutes        = require('./routes/shop');
 const app               = express();
 const errorController   = require('./controllers/error');
 const mongoConnect      = require('./util/database').mongoConnect;
+const User              = require('./models/user');
 
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use((req, res, next) => {
-    // User.findByPk(1)
-    //     .then(user => {
-    //         req.user = user;
-    //         next();
-    //     })
-    //     .catch(err => console.log(err));
+    User.findById("616f51b4b778803db906a050")
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
     next();
 });
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404Page);
