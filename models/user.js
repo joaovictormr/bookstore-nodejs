307d59b441b1b58ddbcd8f99ff1da1d1e379eaca
Joao Victor Martins <joaovictormr91@gmail.com>
2021-10-25 10:28:59 -0300

    Refactoring delete item from cart

--- models/user.js
+++ models/user.js
@@ -64,6 +64,18 @@ class User {
             });
     }
 
+    deleteItemFromCart(productId) {
+        const updatedCartItems = this.cart.items.filter(item => {
+            return productId.toString() !== item.productId.toString();
+        });
+        return db
+            .collection('users')
+            .updateOne(
+                { _id: new mongodb.ObjectId(this._id) },
+                { $set: { cart: {items: updatedCartItems} } } 
+            );
+    }
+
     static findById(userId) {
         const db = getDb();
         return db.collection('users').findOne({_id: new mongodb.ObjectId(userId)});
