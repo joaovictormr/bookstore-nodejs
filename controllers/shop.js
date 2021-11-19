0529c8c20c006b3e445bfacb3b94a5b3cd2bf796
Joao Victor Martins <joaovictormr91@gmail.com>
2021-11-18 23:56:45 -0300

    Saving cookie at login post

--- controllers/shop.js
+++ controllers/shop.js
@@ -8,7 +8,8 @@ exports.getProducts = (req, res, next) => {
             res.render('shop/product-list', {
                 prods: products,
                 pageTitle: 'All Products',
-                path: '/products'
+                path: '/products',
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch(err => console.log(err));
@@ -21,7 +22,8 @@ exports.getProduct = (req, res, next) => {
             res.render('shop/product-detail', {
                 product: product,
                 pageTitle: product.title,
-                path: '/products'
+                path: '/products',
+                isAuthenticated: req.isLoggedIn
             })
         })
         .catch(err => console.log(err));
@@ -33,7 +35,8 @@ exports.getIndex = (req, res, next) => {
             res.render('shop/index', {
                 prods: products,
                 pageTitle: 'Shop',
-                path: '/'
+                path: '/',
+                isAuthenticated: req.isLoggedIn
             })
         })
         .catch(err => console.log(err));
@@ -47,7 +50,8 @@ exports.getCart = (req, res, next) => {
             res.render('shop/cart', {
                 pageTitle: 'Your Cart',
                 path: '/cart',
-                products: products
+                products: products,
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch(err => console.log(err));
@@ -78,7 +82,8 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         path: '/checkout',
-        pageTitle: 'Checkout'
+        pageTitle: 'Checkout',
+        isAuthenticated: req.isLoggedIn
     });
 };
 
@@ -113,7 +118,8 @@ exports.getOrders = (req, res, next) => {
             res.render('shop/orders', {
                 path: '/orders',
                 pageTitle: 'Orders',
-                orders: orders
+                orders: orders,
+                isAuthenticated: req.isLoggedIn
             })
         })
         .catch(err => console.log(err));
